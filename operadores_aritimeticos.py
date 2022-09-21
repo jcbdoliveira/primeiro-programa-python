56bf3510ff51e4c4a4c010b5f8b94727d60e08f2
jcbdoliveira <julio@adminformatica.com.br>
2022-09-20 21:24:15 -0300

    Operadores em Python

---
+++ operadores_aritimeticos.py
@@ -0,0 +1,9 @@
+produto_1 = 10
+produto_2 = 20
+print(f"adição: ", produto_1 + produto_2)
+print(f"subtração: ", produto_1 - produto_2)
+print(f"multiplicação: ", produto_1 * produto_2)
+print(f"divisão: ", produto_2 + produto_1)
+print(f"divisão inteiro: ", produto_2 // produto_1)
+print(f"módulo: ", produto_2 % produto_1)
+print(f"exponenciaçao: ", produto_2 ** produto_1)
---
+++ operadores_de_comparacao.py
@@ -0,0 +1,9 @@
+saldo = 450
+saque = 200
+
+print(saldo == saque)
+print(saldo != saque)
+print(saldo > saque)
+print(saldo >= saque)
+print(saldo < saque)
+print(saldo <= saque)
